c1b10de787eed3507e9c8ca27a57cc168eee6150
ysu-gjx <252932110@qq.com>
2020-09-02 17:43:45 +0800

    添加icon

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,26 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="`icon-${iconPosition}`">
+    <svg class="icon" v-if="icon">
+      <use :xlink:href="`#yg-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
   export default {
-
+    // props: ['icon', 'iconPosition']
+    props: {
+      icon: String,
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator (value) {
+          return value === 'left' || value === 'right'
+        }
+      }
+    }
   }
 </script>
 <style scoped lang="scss">
@@ -14,14 +31,18 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
-    &:hover {
-      border-color: var(--border-color-hover)
-    }
-    &:active {
-      background-color: var(--button-active-bg)
-    }
-    &:focus {
-      outline: none
+    vertical-align: middle;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    &:hover {border-color: var(--border-color-hover)}
+    &:active {background-color: var(--button-active-bg)}
+    &:focus {outline: none}
+    > .icon {order: 1;margin-right: .43em;}
+    > .content {order: 2}
+    &.icon-right {
+      > .icon{order: 2;margin-right: 0;margin-left: .43em}
+      > .content{order: 1;}
     }
   }
   
