d72dd2238edb17006ef161dd6525c11af89c0bfe
ysu-gjx <252932110@qq.com>
2020-09-03 17:10:02 +0800

    添加 chai  expect 测试

--- src/app.js
+++ src/app.js
@@ -23,3 +23,94 @@ var vm = new Vue({
 console.log(vm)
 const el = document.querySelector('#app')
 console.log(el)
+
+
+// 单元测试
+import chai from 'chai'
+const expect = chai.expect
+
+{
+  const Constructor = Vue.extend(GButton)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#yg-settings')
+
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(GButton)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#yg-loading')
+
+  vm.$destroy()
+}
+
+{
+  const oDiv = document.createElement('div')
+  document.body.appendChild(oDiv)
+
+  const Constructor = Vue.extend(GButton)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount(oDiv)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const oDiv = document.createElement('div')
+  document.body.appendChild(oDiv)
+
+  const Constructor = Vue.extend(GButton)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  button.$mount(oDiv)
+  let svg = button.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+
+  button.$el.remove()
+  button.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(GButton)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function () {
+    expect(1).to.eq(1)
+  })
+
+  const btn = vm.$el
+  btn.click()
+}
